ce374ba987547d47fe7549fea96c4b7f22fc644d
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-09 11:07:32 -0600

    wip axios post and get

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import FormikForm from './Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <FormikForm />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <FormikForm />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        <FormikForm />
+      </div>
+    )
+  }
 }
 
 export default App;
--- client/src/Form.js
+++ client/src/Form.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import User from "./User";
+import axios from 'axios';
 
-function RegistrationForm() {
+function RegistrationForm({ values, errors, touched, status }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
+
   return (
-    <Form>
-      <Field type="text" name="username" placeholder="Username" />
-      <Field type="password" name="password" placeholder="Password" />
-      <button>Submit!</button>
-    </Form>
+    <div>
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <button>Submit!</button>
+      </Form>
+      {users.map(user => (
+        <User username={user.username} />
+      ))}
+    </div>
   );
 }
 
-  const FormikForm = withFormik({
-    mapPropsToValues({ username, password }) {
-      return {
-        username: username || "",
-        password: password || ""
-      };
-    },
-  
-    handleSubmit(values) {
-      console.log(values);
-      //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
-    }
-  })(RegistrationForm);
-  
+const FormikForm = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required(),
+    password: Yup.string()
+      .min(8)
+      .required()
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    // console.log(values);
+    // setStatus(values);
+    axios.post('http://localhost:5000/api/register', {
+        username: values.username,
+        password: values.password
+    })
+    .then((response)=>{
+        console.log('RESPONSE', response)
+    })
+  }
+})(RegistrationForm);
 
 export default FormikForm;
